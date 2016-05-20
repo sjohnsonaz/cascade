735d9cec37333bea611a79eec6206ca3b1e99248
Sean Johnson <sjohnson@sjohnsonaz.com>
2016-05-20 12:22:13 -0700

    Updated Graph.

---
+++ src/graph/Define.js
@@ -0,0 +1,29 @@
+var Define = (function () {
+    function Define() {
+
+    }
+
+    Define.prototype = {
+
+    };
+
+    Define.extend = function ($constructor, $parent, $prototype) {
+        return $constructor.prototype = Object.assign(Object.create($parent.prototype, {
+            constructor: {
+                value: $constructor,
+                writable: true,
+                configurable: true
+            }
+        }), $prototype);
+    };
+
+    Define.super = function ($constructor, thisArg) {
+        $constructor.apply(thisArg, Array.prototype.splice.call(arguments, 0, 2));
+    };
+
+    Define.superConstructor = function (thisArg) {
+        arguments.callee.caller.prototype.constructor.apply(thisArg, Array.prototype.splice.call(arguments, 0, 1));
+    };
+
+    return Define;
+})();
--- src/graph/Graph.js
+++ src/graph/Graph.js
@@ -7,21 +7,113 @@ var Graph = (function () {
 
     };
 
+    Graph.computedContexts = [];
+
     Graph.observable = function (value) {
+        var context = Graph.computedContexts[Graph.computedContexts.length - 1];
+        context.references.push(this); // TODO: confirm this
+    };
+
+    Graph.computed = function (definition) {
 
     };
 
     return Graph;
 })();
 
-var Observable = (function () {
-    function Observable() {
-
+var Subscribable = (function () {
+    function Subscribable(value) {
+        this.value = value;
+        this.subscribers = [];
     }
 
-    Observable.prototype = {
+    Subscribable.prototype = {
+        subscribe: function (callback) {
+            if (callback) {
+                this.subscribers.push(callback);
+                callback(this.value);
+            }
+        },
+        unsubscribe: function (callback) {
+            if (callback) {
+                var index = this.subscribers.indexOf(callback);
+                if (index >= 0) {
+                    this.subscribers.splice(index, 1);
+                }
+            }
+        },
+        publish: function () {
+            for (var index = 0, length = this.subscribers.length; index < length; index++) {
+                var subscriber = this.subscribers[index];
+                subscriber(this.value);
+            }
+        }
+    };
+
+    return Subscribable;
+})();
+
+var Observable = (function () {
+    function Observable() {
+        Define.super(Subscribable, this);
+    }
+
+    Define.extend(Observable, Subscribable, {
+        getValue: function () {
+            var context = Graph.computedContexts[Graph.computedContexts.length - 1];
+            if (context) {
+                context.references.push(this);
+            }
+            return this.value;
+        },
+        setValue: function (value) {
+            this.value = value;
+            this.publish();
+        }
+    });
+
+    return Observable;
+})();
+
+var Computed = (function () {
+    function Computed(definition) {
+        Define.super(Subscribable, this);
+
+        this.subscriptions = [];
+        this.definition = definition;
+        this.runDefinition(definition);
+
+        return this.value;
+    }
+
+    Define.extend(Computed, Subscribable, {
+        runDefinition: function (definition) {
+            for (var index = 0, length = this.subscriptions.length; index < length; index++) {
+                var subscription = this.subscriptions[index];
+            }
+
+            var context = new ComputedContext();
+            Graph.computedContexts.push(context);
+            this.value = definition();
+            Graph.computedContexts.pop();
+
+            for (var index = 0, length = context.references.length; index < length; index++) {
+                var reference = context.references[index];
+            }
+        }
+    });
+
+    return Computed;
+})();
+
+var ComputedContext = (function () {
+    function ComputedContext() {
+        this.references = [];
+    }
+
+    ComputedContext.prototype = {
 
     };
 
-    return Observable;
+    return ComputedContext;
 })();
