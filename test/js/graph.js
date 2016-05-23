dc2568ce1e88d77617e7553a167ada38947bea7a
Sean Johnson <sjohnson@sjohnsonaz.com>
2016-05-23 14:02:14 -0700

    Created publish test.

--- test/js/graph.js
+++ test/js/graph.js
@@ -3,12 +3,29 @@ window.onload = function () {
         var self = this;
         Graph.createObservable(this, 'a', 1);
         Graph.createObservable(this, 'b', 2);
-        Graph.createComputed(this, 'computed', function () {
+        Graph.createObservable(this, 'c', 3);
+        Graph.createComputed(this, 'ab', function () {
             return self.a + self.b;
         });
-        this._graph.subscribables.computed.subscribe(function (value) {
-            console.log(value);
+        Graph.createComputed(this, 'bc', function () {
+            return self.b + self.c;
         });
-        this.a = 10;
+        Graph.createComputed(this, 'cd', function () {
+            return self.c + self.d;
+        });
+        Graph.createComputed(this, 'ad', function () {
+            return self.a + self.d;
+        });
+        Graph.createComputed(this, 'abc', function () {
+            return self.ab + self.bc + self.cd + self.ad;
+        });
+        var runs = 0;
+        this._graph.subscribables.abc.subscribe(function (value) {
+            runs++;
+        });
+        this.a = 11;
+        this.b = 12;
+        this.c = 13;
+        console.log(runs);
     }
 };
