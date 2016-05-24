b71501635e7f128f41828c5faa070b2662fc3603
Sean Johnson <sjohnson@sjohnsonaz.com>
2016-05-23 17:11:18 -0700

    Added Graph tests.

--- test/js/graph.js
+++ test/js/graph.js
@@ -1,39 +1,95 @@
 window.onload = function () {
-    window.viewModel = new function () {
-        var self = this;
-        this.runs = 0;
-        Graph.createObservable(this, 'a', 1);
-        Graph.createObservable(this, 'b', 2);
-        Graph.createObservable(this, 'c', 3);
-        Graph.createObservable(this, 'd', 4);
-        Graph.createComputed(this, 'ab', function () {
-            return self.a + self.b;
-        });
-        Graph.createComputed(this, 'ac', function () {
-            return self.a + self.c;
-        });
-        Graph.createComputed(this, 'ad', function () {
-            return self.a + self.d;
-        });
-        Graph.createComputed(this, 'bc', function () {
-            return self.b + self.c;
-        });
-        Graph.createComputed(this, 'bd', function () {
-            return self.b + self.d;
-        });
-        Graph.createComputed(this, 'cd', function () {
-            return self.c + self.d;
-        });
-        Graph.createComputed(this, 'abcd', function () {
-            return self.ab + self.ac + self.ad + self.bc + self.bd + self.cd;
-        });
-        this._graph.subscribables.abcd.subscribe(function (value) {
-            self.runs++;
-            console.log('Value: ' + value + ', Runs: ' + self.runs);
-        });
-        this.a = 11;
-        this.b = 12;
-        this.c = 13;
-        this.d = 14;
-    }
+    runAllTests([test0, test1], function (output) {
+        window.tests = output;
+    })
 };
+
+function runAllTests(tests, callback) {
+    var index = 0;
+    var output = [];
+    runTest(tests, index, function iterate(data) {
+        output.push(data);
+        index++;
+        if (index < tests.length) {
+            runTest(tests, index, iterate);
+        } else {
+            callback(output);
+        }
+    });
+}
+
+function runTest(tests, index, callback) {
+    tests[index](callback);
+}
+
+function test0(callback) {
+    console.log('test 0');
+    var viewModel = {};
+    viewModel.runs = 0;
+    Graph.createObservable(viewModel, 'a', 1);
+    Graph.createObservable(viewModel, 'b', 2);
+    Graph.createObservable(viewModel, 'c', 3);
+    Graph.createObservable(viewModel, 'd', 4);
+    Graph.createComputed(viewModel, 'ab', function () {
+        return viewModel.a + viewModel.b;
+    });
+    Graph.createComputed(viewModel, 'ac', function () {
+        return viewModel.a + viewModel.c;
+    });
+    Graph.createComputed(viewModel, 'ad', function () {
+        return viewModel.a + viewModel.d;
+    });
+    Graph.createComputed(viewModel, 'bc', function () {
+        return viewModel.b + viewModel.c;
+    });
+    Graph.createComputed(viewModel, 'bd', function () {
+        return viewModel.b + viewModel.d;
+    });
+    Graph.createComputed(viewModel, 'cd', function () {
+        return viewModel.c + viewModel.d;
+    });
+    Graph.createComputed(viewModel, 'abcd', function () {
+        return viewModel.ab + viewModel.ac + viewModel.ad + viewModel.bc + viewModel.bd + viewModel.cd;
+    });
+    var complete = false;
+    viewModel._graph.subscribables.abcd.subscribe(function (value) {
+        viewModel.runs++;
+        if (complete) {
+            console.log('Value: ' + value + ', Runs: ' + viewModel.runs);
+            callback(viewModel);
+        }
+    });
+    viewModel.a = 11;
+    viewModel.b = 12;
+    viewModel.c = 13;
+    viewModel.d = 14;
+    complete = true;
+}
+
+function test1(callback) {
+    console.log('test 1');
+    var viewModel = {};
+    viewModel.runs = 0;
+    Graph.createObservable(viewModel, 'a', 1);
+    Graph.createObservable(viewModel, 'b', 2);
+    Graph.createObservable(viewModel, 'c', 3);
+    Graph.createComputed(viewModel, 'ab', function () {
+        return viewModel.a + viewModel.b;
+    });
+    Graph.createComputed(viewModel, 'bc', function () {
+        return viewModel.b + viewModel.c;
+    });
+    Graph.createComputed(viewModel, 'aab', function () {
+        return viewModel.a + viewModel.ab;
+    });
+    var complete = false;
+    viewModel._graph.subscribables.aab.subscribe(function (value) {
+        viewModel.runs++;
+        if (complete) {
+            console.log('Value: ' + value + ', Runs: ' + viewModel.runs);
+            callback(viewModel);
+        }
+    });
+    viewModel.a = 11;
+    complete = true;
+}
