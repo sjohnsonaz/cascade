eb544bc1b721703517e0a7d341db46e1ef2b6b19
Sean Johnson <sjohnson@sjohnsonaz.com>
2016-06-09 11:39:28 -0700

    Updated Cascade tests to not affect one another.

--- test/js/cascade/test0.js
+++ test/js/cascade/test0.js
@@ -2,20 +2,12 @@ TestRunner.test({
     name: 'Text can be parsed into a Template and fragment',
     test: function (input, callback) {
         var template = new Template('\r\n\
-            <h1>Widget Demo</h1>\r\n\
-            @if: false {\r\n\
-                <div id="testFalse">boolean constant false</div>\r\n\
-            }\r\n\
-            @if: true {\r\n\
-                <div id="testTrue">boolean constant true</div>\r\n\
-        	}\r\n\
+            <div id="testNode">Test Node</div>\r\n\
         ');
         callback(template.build());
     },
     assert: function (result, callback) {
-        document.body.appendChild(result);
-        var testFalse = document.getElementById("testFalse");
-        var testTrue = document.getElementById("testTrue");
-        callback(testTrue && !testFalse);
+        var testNode = result.getElementById("testNode");
+        callback(!!testNode);
     }
 });
---
+++ test/js/cascade/test1.js
@@ -0,0 +1,20 @@
+TestRunner.test({
+    name: 'Conditional bindings control node creation.',
+    test: function (input, callback) {
+        var template = new Template('\r\n\
+            <h1>Widget Demo</h1>\r\n\
+            @if: false {\r\n\
+                <div id="testFalse">boolean constant false</div>\r\n\
+            }\r\n\
+            @if: true {\r\n\
+                <div id="testTrue">boolean constant true</div>\r\n\
+        	}\r\n\
+        ');
+        callback(template.build());
+    },
+    assert: function (result, callback) {
+        var testFalse = result.getElementById("testFalse");
+        var testTrue = result.getElementById("testTrue");
+        callback(testTrue && !testFalse);
+    }
+});
