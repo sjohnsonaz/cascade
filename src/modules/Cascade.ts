a591a73a891953f3e387efcac606a6386427d76f
Sean Johnson <sjohnson@sjohnsonaz.com>
2016-06-21 16:08:20 -0700

    Cleaned module.

--- src/modules/Cascade.ts
+++ src/modules/Cascade.ts
@@ -1,14 +1,5 @@
-/*
-import VirtualNode from '../cascade/VirtualNode';
-import Observable from '../graph/Observable';
-import Computed from '../graph/Computed';
-import Graph from '../graph/Graph';
-
-module.exports = Cascade;
-*/
-
 export {default as default} from '../cascade/Cascade';
-export {default as VirtualNode} from '../cascade/VirtualNode';
 export {default as Observable} from '../graph/Observable';
 export {default as Computed} from '../graph/Computed';
 export {default as Graph} from '../graph/Graph';
+export {default as VirtualNode} from '../cascade/VirtualNode';
