4a444d2c0180b31b7f014f0797ee7995d5c3ff8b
Sean Johnson <sjohnson@sjohnsonaz.com>
2016-07-14 15:52:35 -0700

    Moved Graph methods back to Graph.

--- src/scripts/modules/Cascade.ts
+++ src/scripts/modules/Cascade.ts
@@ -1,5 +1,5 @@
-export {default as default, observable, computed} from '../cascade/Cascade';
+export {default as default} from '../cascade/Cascade';
 export {default as Observable} from '../graph/Observable';
 export {default as Computed} from '../graph/Computed';
-export {default as Graph} from '../graph/Graph';
+export {default as Graph, observable, computed} from '../graph/Graph';
 export {default as VirtualNode} from '../cascade/VirtualNode';
