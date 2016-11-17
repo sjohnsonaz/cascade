056d85cd7e99e2c0add5a51a01cf936a5818ea2e
Sean Johnson <sjohnson@sjohnsonaz.com>
2016-11-16 19:14:22 -0700

    Removed unused file.

--- src/scripts/cascade/TemporaryNode.ts
+++
@@ -1,7 +0,0 @@
-export default class TemporaryNode<T extends Object> {
-    type: string;
-    properties: T;
-    children: Array<TemporaryNode<any> | string>;
-    constructor(type: string, properties?: T, ...children: Array<TemporaryNode<any> | string>) {
-    }
-}
