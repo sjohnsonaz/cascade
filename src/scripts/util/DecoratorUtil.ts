c844cd5c5917bcd03fbd6e100e25463fe29114fc
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-06-27 11:59:28 -0700

    Added minLength example to DecoratorUtil.

--- src/scripts/util/DecoratorUtil.ts
+++ src/scripts/util/DecoratorUtil.ts
@@ -1,27 +1,41 @@
 import Graph from '../graph/Graph';
 import { IObservable } from '../graph/IObservable';
+import Computed from '../graph/Computed';
 
+type ObservableFactory<T> = {
+    (value: T, thisArg: any): IObservable<T>;
+}
 export default class DecoratorUtil {
-    static createObservableIfNotExists<T>(obj: any, property: string, factory: (value?: T) => IObservable<T>, value?: T, set?: boolean): IObservable<T> {
+    static createObservableIfNotExists<T>(obj: any, property: string, factory: ObservableFactory<T>, value?: T, set?: boolean, thisArg?: any): IObservable<T> {
         Graph.attachGraph(obj);
         if (!obj._graph.observables[property]) {
-            obj._graph.observables[property] = factory(value);
+            obj._graph.observables[property] = factory(value, thisArg);
         } else if (set) {
             obj._graph.observables[property].setValue(value);
         }
         return obj._graph.observables[property];
     }
 
-    static attachObservable<T>(target: any, propertyKey: string, factory: (value?: T) => IObservable<T>) {
+    static attachObservable<T>(target: any, propertyKey: string, factory: ObservableFactory<T>, readOnly: boolean = false) {
         Object.defineProperty(target, propertyKey, {
             enumerable: true,
             configurable: true,
             get: function () {
-                return DecoratorUtil.createObservableIfNotExists(this, propertyKey, factory).getValue();
+                return DecoratorUtil.createObservableIfNotExists(this, propertyKey, factory, undefined, false, this).getValue();
             },
-            set: function (value: T) {
-                DecoratorUtil.createObservableIfNotExists(this, propertyKey, factory, value, true);
+            set: readOnly ? undefined : function (value: T) {
+                DecoratorUtil.createObservableIfNotExists(this, propertyKey, factory, value, true, this);
             }
         });
+    }
+}
+
+export function minLength(length: number = 0): any {
+    return function minLength(target: any, propertyKey: string, descriptor?: TypedPropertyDescriptor<boolean>): any {
+        DecoratorUtil.attachObservable<boolean>(target, propertyKey + '_minLength', (value: boolean, thisArg: any) => {
+            return new Computed<boolean>(function () {
+                return target[propertyKey].length >= length;
+            }, false, thisArg);
+        }, true);
     }
 }
