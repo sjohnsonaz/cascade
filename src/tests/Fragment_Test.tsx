36ecbae0fa8fede219e4ab05932c1f4b4c8749c2
Sean Johnson <sjohnson@sjohnsonaz.com>
2018-02-14 17:25:26 -0700

    Added Fragment diff test.

--- src/tests/Fragment_Test.tsx
+++ src/tests/Fragment_Test.tsx
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 
-import Cascade, { VirtualNode, Component } from '../scripts/modules/Cascade';
+import Cascade, { VirtualNode, Component, observable } from '../scripts/modules/Cascade';
 
 describe('Fragment.toNode', function () {
     it('should render Fragment Nodes', () => {
@@ -14,5 +14,56 @@ describe('Fragment.toNode', function () {
             let div: HTMLElement = rootElement.childNodes[0] as any;
             expect(div.id).to.equal('testId');
         });
+    });
+
+    it('should be able to Diff', (done) => {
+        class ViewModel {
+            @observable value = false;
+        }
+        interface IViewProps {
+            viewModel: ViewModel;
+        }
+        class WithFragments extends Component<any> {
+            render() {
+                return (
+                    <>
+                        <div>a</div>
+                        <div>b</div>
+                    </>
+                );
+            }
+        }
+        class WithoutFragments extends Component<any> {
+            render() {
+                return (
+                    <div>c</div>
+                );
+            }
+        }
+        class View extends Component<IViewProps> {
+            render() {
+                return (
+                    <span>
+                        {this.props.viewModel.value ?
+                            <WithoutFragments /> :
+                            <WithFragments />
+                        }
+                    </span>
+                );
+            }
+        }
+        var viewModel = new ViewModel();
+        var container = document.createElement('div')
+        Cascade.render(container, <View viewModel={viewModel} />);
+        viewModel.value = true;
+        window.setTimeout(() => {
+            let span = container.childNodes[0];
+            let div = span.childNodes[0];
+            let text = div.textContent;
+            expect(span.childNodes.length).to.equal(1);
+            expect(div.childNodes.length).to.equal(1);
+            expect(text).to.equal('c');
+            done();
+        }, 20);
     });
 });
